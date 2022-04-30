cc1807766f0d419cc423d8a127e4a87d81be5789
sp-tam1m <white.hat.tamim@gmail.com>
2022-04-30 16:31:18 +0600

    bikedb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = 5000;
@@ -11,6 +12,24 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('backend')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.76mh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const bikes = client.db('bikedb').collection('bikes');
+        console.log('bikedb conected');
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.listen(port, () => {
