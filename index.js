ec0138a8384c473a17b49d12b5bf30914953d0c8
sp-tam1m <white.hat.tamim@gmail.com>
2022-05-07 07:04:00 +0600

    complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('backend')
+    res.send('Backend Runnign')
 })
 
 
